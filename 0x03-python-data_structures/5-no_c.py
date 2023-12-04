0614e664c10c0a29e77dafc61503603912c5f2b4
Amanda Mabunda <amandamabu14@gmail.com>
2023-12-04 05:40:32 -0800

    correcting errors

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_string = ""
-    for element in my_string:
-        if element != 'c' and  element != 'C':
-            new_string += element
-    return new_string
+    for a in my_string:
+        if (a != 'c') and  (a != 'C'):
+            new_string += a
+    return(new_string)
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        sentence = None
-        if sentence:
-            sent_len = len(sentence)
-        else:
-            sent_len = 0 
-            return (sent_len, sentence if not sentence else sentence[:1])
+     length = len(sentence)
+     first_char = sentence[0] if length > 0 else "None"
+     tup = length, first_char
+      return(tup)
