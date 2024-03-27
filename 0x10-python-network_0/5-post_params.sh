ad0058d981009b8279dcd4135733ed4bdca8fb52
Amanda Mabunda <amandamabu14@gmail.com>
2024-03-27 08:11:03 -0700

    my bash script files

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#script that takes in a URL,
+#sends a POST request to the passed URL,
+#and displays the body of the response
+
+curl -sX POST $1 -d "emial=test@gmail.com&subject=I will always be here for PLD" -L
